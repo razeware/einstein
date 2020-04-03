6d89bd14477da74bc5e55da38969dbef5315df64
Sam Davies <sam@razeware.com>
2020-04-03 09:38:28 +0100

    CI is non-interactive

--- bin/build_prod.sh
+++ bin/build_prod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build -t einstein-atoe .
-docker run -v "$(pwd)/dist:/var/www/einstein/dist" -it --rm einstein-atoe yarn run build
+docker run -v "$(pwd)/dist:/var/www/einstein/dist" --rm einstein-atoe yarn run build
